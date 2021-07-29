5453db8648ab08f47a51ea7093adb1d2c1cc2d1f
pc-v2 <meguminxkazuma@protonmail.com>
2021-07-29 13:22:35 +0700

    initialize

---
+++ 1. Pengenalan/src/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int x;
+    cout << "Masukan Bilangan Bulat: ";
+    cin >> x;
+    cout << "Bilangan Bulat Yang Anda Masukan Adalah: " << x << endl;
+    return 0;
+}
